734f8a25d2cd06ca0479a8525bcee5926d7d7bf2
Quan Nt <quannt210399@gmail.com>
2023-04-18 09:12:14 +0700

    #16 useNavigate

--- src/views/Blog/DetailBlog.js
+++ src/views/Blog/DetailBlog.js
@@ -1,9 +1,24 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 
 const DetailBlog = () => {
     let { id } = useParams();
-    console.log(">>> check use param: ", useParams());
-    return <h1> Hello detail blogs with id = {id}</h1>;
+    const navigate = useNavigate();
+
+    const handleBackData = () => {
+        navigate("/blog");
+    };
+
+    // console.log(">>> check use param: ", useParams());
+
+    return (
+        <>
+            <div>
+                {" "}
+                <span onClick={handleBackData}>&lt;-- Back </span>
+            </div>
+            <h1> Hello detail blogs with id = {id}</h1>
+        </>
+    );
 };
 
 export default DetailBlog;
